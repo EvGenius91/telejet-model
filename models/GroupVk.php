a0b285e3a0dd40ea1fc7a67528a605397c9511ce
Evgeniy Stepin <mail@estepin.ru>
2022-03-21 12:08:01 +0400

    First commit

--- models/GroupVk.php
+++ models/GroupVk.php
@@ -5,10 +5,12 @@
  * @property StatisticData $statisticData
  * @property StatisticData $postStat
  * @property StatisticData $commentStat
+ * @property StatisticData $likeStat
  */
 class GroupVk extends GroupBase
 {
     public $subscriberStat;
     public $postStat;
     public $commentStat;
+    public $likeStat;
 }
